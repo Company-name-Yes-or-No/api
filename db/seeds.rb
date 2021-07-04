4eb26455bb89c8106b7a375ebebdf13ff7229326
jiko797torayo <oec7dwayne7kj@gmail.com>
2021-07-05 00:25:08 +0900

    feat: CompanyのCRUD追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Company.create!(
+  [
+    {
+      name: '株式会社にほん'
+    },
+    {
+      name: '株式会社あめりか'
+    },
+    {
+      name: '株式会社おーすとらりあ'
+    },
+    {
+      name: '株式会社いぎりす'
+    },
+    {
+      name: '株式会社けにあ'
+    },
+  ]
+)
